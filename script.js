33d74842f7c177c8dd63058e6a51744b0a64323c
Zeeshan <mzeeshi187@gmail.com>
2024-11-18 21:19:05 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+// let BtnClicked=(a,b)=>{
+//     console.log("function called");
+// }
+// BtnClicked(20,30)
+
+// template litteral
+// name = "Zeeshan";
+// age = 10;
+// console.log(`My name is ${name} and My age is ${age}`);
+
+// if (age < 18) {
+//   console.log("age is less than 18");
+// } else {
+//   console.log("age is greater thn 18");
+// }
+
+// age<18?console.log("age is less than 18"):
+// age>18?console.log("age is greater than 18"):
+// console.log("age is equal to 18")
+
+// console.log(age<18?"age is less than 18":"age is greater than 18");
+
+
+// let backendData=["Zeeshan","Data",20,30,40,50]
+
+
+// console.log(...backendData)
+
+
+// let Student = (name,data, ...marks) => {
+//   console.log(marks);
+// };
+
+
+// Student(...backendData);
+
+
+
+
+// call function==> ...(spread operator)
+// argument get-> function==> ...(rest operator)
+
+
+
